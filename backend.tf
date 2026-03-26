349ba30bf9c66715c105e3b2a09c99218988bf33
ShaileshKalyane <Omprakashkalyane475@gmail.com>
2026-03-27 00:19:11 +0530

    updated the region to ap-south-1

--- backend.tf
+++ backend.tf
@@ -1,12 +1,12 @@
 provider "aws"{
-    region="us-east-1"
+    region="ap-south-1"
 }
 
 terraform {
   backend "s3" {
     bucket       = "shailesh-temp-26032026"
     key          = "terraformstate.tfstate"
-    region       = "us-east-1"
+    region       = "ap-south-1"
     encrypt      = true
     use_lockfile = true
   }
